e2c09e212f8093179b07771efcba65404242b56a
Sylum Mastropaolo <Sylum.Mastropaolo@gmail.com>
2021-03-11 00:27:13 -0500

    Some base logic and disply created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h3>
+      Warlock
+    </h3>
+    <div v-for="slot in warlock.spellSlots" :key="slot.label">
+      <h5>
+        {{slot.current}} / {{slot.max}} {{slot.label}}
+      </h5>
+      <button @click="slot.current--">-</button>
+      <button @click="slot.current++">+</button>
+    </div>
+    <h3>
+      Sorcerer
+    </h3>
+    <h5>
+      {{sorcerer.currentSorcPoints}} / {{sorcerer.maxSorcPoints}} Sorcerer Points
+    </h5>
+    <button @click="sorcerer.currentSorcPoints--">-</button>
+    <button @click="sorcerer.currentSorcPoints++">+</button>
+
+    <div v-for="slot in sorcerer.spellSlots" :key="slot.label">
+      <h5>
+        {{slot.current}} / {{slot.max}} {{slot.label}}
+      </h5>
+      <button @click="slot.current--">-</button>
+      <button @click="slot.current++">+</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      warlock: {
+        spellSlots: [
+          {
+            current: 2,
+            max: 2,
+            level: 1,
+            label: "Level 1 Pact Slots"
+          }
+        ],
+        currentSlots: 2,
+        maxSlots: 2,
+        slotsSpellLevel: 1
+      },
+      sorcerer: {
+        currentSorcPoints: 3,
+        maxSorcPoints: 3,
+
+        spellSlots: [
+          {
+            current: 4,
+            max: 4,
+            level: 1,
+            label: "Level 1 Spell Slots"
+          },
+          {
+            current: 2,
+            max: 2,
+            level: 2,
+            label: "Level 2 Spell Slots"
+          }
+        ]
+      },
+      pactSlots: 2,
+      maxPactSlots: 2,
+    }
+  },
+  methods: {
+    convertToSorcPoints(index, slots) {
+      slots[index].current--;
+      slots[index].level;
+    }
+  },
+  computed: {
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 
+Vue.use(Vuex);
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  }
+})
+
 new Vue({
+  store: store,
   render: h => h(App),
 }).$mount('#app')
